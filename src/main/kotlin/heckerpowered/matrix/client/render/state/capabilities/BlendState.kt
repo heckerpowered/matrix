db8ee1139de7b1b63b55e3753ff635c3be339c15
heckerpowered <72550834+heckerpowered@users.noreply.github.com>
2025-07-21 19:48:26 +0800

    Support capture snapshot of blend state

--- src/main/kotlin/heckerpowered/matrix/client/render/state/capabilities/BlendState.kt
+++ src/main/kotlin/heckerpowered/matrix/client/render/state/capabilities/BlendState.kt
@@ -1,6 +1,7 @@
 package heckerpowered.matrix.client.render.state.capabilities
 
 import org.lwjgl.opengl.GL11.GL_BLEND
+import org.lwjgl.opengl.GL11.glIsEnabled
 
 class BlendState(enabled: Boolean) : CapabilityState(GL_BLEND, enabled) {
     companion object {
