2004871092d1d5688783b7068d094230c79fc91c
heckerpowered <72550834+heckerpowered@users.noreply.github.com>
2025-07-21 19:47:37 +0800

    Support capture snapshot of blend state

--- src/main/kotlin/heckerpowered/matrix/client/render/state/capabilities/BlendState.kt
+++ src/main/kotlin/heckerpowered/matrix/client/render/state/capabilities/BlendState.kt
@@ -2,5 +2,13 @@ package heckerpowered.matrix.client.render.state.capabilities
 
 import org.lwjgl.opengl.GL11.GL_BLEND
 
-class BlendState(enabled: Boolean) : CapabilityState(GL_BLEND, enabled)
+class BlendState(enabled: Boolean) : CapabilityState(GL_BLEND, enabled) {
+    companion object {
+        fun captureSnapshot(): BlendState {
+            val previousBlendState = glIsEnabled(GL_BLEND)
+            val snapshot = BlendState(previousBlendState)
+            return snapshot
+        }
+    }
+}
 
