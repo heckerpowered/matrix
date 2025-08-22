10a0413f4eaf75fcbddecfa26aaf30cfe90e353f
heckerpowered <72550834+heckerpowered@users.noreply.github.com>
2025-08-22 19:08:22 +0800

    refactor: Remove PostProcessShader class

--- src/main/kotlin/heckerpowered/matrix/client/shader/PostProcessShader.kt
+++
@@ -1,6 +0,0 @@
-package heckerpowered.matrix.client.shader
-
-class PostProcessShader(vertex: String, fragment: String, uniform: Array<UniformProvider> = emptyArray()) :
-    BlitShader(vertex, fragment, uniform) {
-    var enabled = true
-}
