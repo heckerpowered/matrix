28398e1a33c7480d99a871ed46668aa7360bfe9a
heckerpowered <72550834+heckerpowered@users.noreply.github.com>
2025-10-04 18:53:58 +0800

    feat(common/magic): add GameTick value class

---
+++ src/main/kotlin/heckerpowered/matrix/common/magic/GameTick.kt
@@ -0,0 +1,14 @@
+/*
+ * SPDX-License-Identifier: MIT
+ * Copyright (c) 2025 heckerpowered
+ */
+
+package heckerpowered.matrix.common.magic
+
+@JvmInline
+value class GameTick(val ticks: Long) {
+    companion object {
+        val Number.ticks: GameTick
+            get() = GameTick(this.toLong())
+    }
+}
