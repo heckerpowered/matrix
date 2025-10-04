8c09a7848e145d21e15df497c7bd851df3b735b6
heckerpowered <72550834+heckerpowered@users.noreply.github.com>
2025-10-04 18:53:46 +0800

    feat(persistent): Add SingleSlotNbtDecoder abstract class

---
+++ src/main/kotlin/heckerpowered/matrix/common/persistent/serialization/SingleSlotNbtDecoder.kt
@@ -0,0 +1,17 @@
+/*
+ * SPDX-License-Identifier: MIT
+ * Copyright (c) 2025 heckerpowered
+ */
+
+package heckerpowered.matrix.common.persistent.serialization
+
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.encoding.CompositeDecoder
+
+abstract class SingleSlotNbtDecoder(codec: NbtCodec) : NbtDecoder(codec) {
+    private var consumed = false
+    override fun decodeElementIndex(descriptor: SerialDescriptor): Int =
+        if (!consumed) {
+            consumed = true; 0
+        } else CompositeDecoder.DECODE_DONE
+}
