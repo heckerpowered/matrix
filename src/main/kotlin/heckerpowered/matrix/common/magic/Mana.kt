88954b9561a7a9f9d2f437b177adeda1c5c5a000
heckerpowered <72550834+heckerpowered@users.noreply.github.com>
2026-01-27 15:12:06 +0530

    refactor(matrix): Update Mana type to use Double and add arithmetic operations

--- src/main/kotlin/heckerpowered/matrix/common/magic/Mana.kt
+++ src/main/kotlin/heckerpowered/matrix/common/magic/Mana.kt
@@ -1,14 +1,20 @@
 /*
  * SPDX-License-Identifier: MIT
- * Copyright (c) 2025 heckerpowered
+ * Copyright (c) 2026 heckerpowered
  */
 
 package heckerpowered.matrix.common.magic
 
 @JvmInline
-value class Mana(val amount: Long) {
+value class Mana(val amount: Double) {
     companion object {
         val Number.mana: Mana
-            get() = Mana(toLong())
+            get() = Mana(toDouble())
+
+        operator fun Mana.plus(other: Mana): Mana = Mana(this.amount + other.amount)
+        operator fun Mana.minus(other: Mana): Mana = Mana(this.amount - other.amount)
+        operator fun Mana.times(other: Mana): Mana = Mana(this.amount * other.amount)
+        operator fun Mana.div(other: Mana): Mana = Mana(this.amount / other.amount)
+        operator fun Mana.compareTo(other: Mana): Int = this.amount.compareTo(other.amount)
     }
 }
