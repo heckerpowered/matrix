7409f9d0590f8f328ac3b0eb721c07d41c9adf31
heckerpowered <72550834+heckerpowered@users.noreply.github.com>
2026-01-27 15:20:29 +0530

    adjust(matrix): Modify DecisiveStrikeMagic to use missing mana for damage calculation and update blood pact check

--- src/main/kotlin/heckerpowered/matrix/common/magic/DecisiveStrikeMagic.kt
+++ src/main/kotlin/heckerpowered/matrix/common/magic/DecisiveStrikeMagic.kt
@@ -1,16 +1,20 @@
 /*
  * SPDX-License-Identifier: MIT
- * Copyright (c) 2025 heckerpowered
+ * Copyright (c) 2026 heckerpowered
  */
 
 package heckerpowered.matrix.common.magic
 
 import heckerpowered.matrix.Matrix
-import heckerpowered.matrix.common.effect.bloodPactActive
+import heckerpowered.matrix.common.effect.isBloodPactActive
 import heckerpowered.matrix.common.magic.GameTick.Companion.ticks
+import heckerpowered.matrix.common.magic.Mana.Companion.div
 import heckerpowered.matrix.common.magic.Mana.Companion.mana
-import heckerpowered.matrix.common.persistent.ChannelQueue
+import heckerpowered.matrix.common.magic.Mana.Companion.minus
+import heckerpowered.matrix.common.persistent.mana
+import heckerpowered.matrix.common.persistent.maxMana
 import heckerpowered.matrix.common.tag.MatrixDamageTypes
+import heckerpowered.matrix.core.extensions.EntityExtensions.damage
 import net.minecraft.entity.LivingEntity
 import net.minecraft.entity.attribute.EntityAttributes
 import net.minecraft.entity.player.PlayerEntity
@@ -25,18 +29,22 @@ object DecisiveStrikeMagic : Magic(
 ) {
     override fun cast(player: ServerPlayerEntity?, target: LivingEntity, sequence: ChannelQueue, data: MagicData) {
         super.cast(player, target, sequence, data)
-        val damageSource = MemoryEraseMagic.getDamageSource(player, target, sequence) { target.world.damageSources.create(MatrixDamageTypes.magic, player) }
+        val damageSource = MemoryWipeMagic.getDamageSource(player, target, data) { target.world.damageSources.create(MatrixDamageTypes.magic, player) }
         target.timeUntilRegen = 0
 
-        // Each consumed mana increases the damage by 1%, up to a maximum of 400%
-        val castedMagicCost = (sequence.castedMagics().sumOf { it.cost }).coerceAtLeast(0)
+        // Each 1% missing mana increases damage by 4%
+        val missingMana = if (player == null) {
+            .0
+        } else {
+            ((player.maxMana - player.mana) / player.maxMana).amount
+        }
 
         val baseDamage = 6.0
         val damageIncreaseBasedOnMaxHealth = (target.maxHealth * 0.14).coerceAtLeast(.0)
         val playerAttackDamage = player?.getAttributeValue(EntityAttributes.GENERIC_ATTACK_DAMAGE) ?: .0
 
-        val damageMultiplierBasedOnCost = (castedMagicCost * 0.01).coerceIn(.0..4.0)
-        val damageMultiplierBloodPact = if (player?.bloodPactActive == true) {
+        val damageMultiplierBasedOnCost = (missingMana * 0.04).coerceIn(.0..4.0)
+        val damageMultiplierBloodPact = if (player?.isBloodPactActive == true) {
             1.0
         } else {
             .0
@@ -45,13 +53,14 @@ object DecisiveStrikeMagic : Magic(
         val damage = baseDamage + damageIncreaseBasedOnMaxHealth + playerAttackDamage
         val amount = damage * (1 + damageMultiplierBasedOnCost + damageMultiplierBloodPact)
 
-        target.damage(damageSource, amount.toFloat())
-        player?.addCritParticles(target)
-        player?.addEnchantedHitParticles(target)
+        if (target.damage(amount.toFloat(), damageSource)) {
+            player?.addCritParticles(target)
+            player?.addEnchantedHitParticles(target)
+        }
     }
 
     override fun availableStatus(player: PlayerEntity, target: LivingEntity?, sequence: ChannelQueue?): MagicAvailableStatus {
-        val damageSource = if (sequence?.sequencedAfter<MemoryEraseMagic>() == true) {
+        val damageSource = if (sequence?.contains<MemoryWipeMagic>() == true) {
             player.world.damageSources.create(MatrixDamageTypes.magic)
         } else {
             player.world.damageSources.create(MatrixDamageTypes.magic, player)
@@ -62,13 +71,5 @@ object DecisiveStrikeMagic : Magic(
         }
 
         return super.availableStatus(player, target, sequence)
-    }
-
-    override fun getChannelTime(player: PlayerEntity, target: LivingEntity, sequence: ChannelQueue?, data: MagicData): Long {
-        val index = sequence?.index ?: 0
-        val channelTime = super.getChannelTime(player, target, sequence, data)
-
-        val reducedChannelTime = (channelTime * (index * 0.2)).toLong()
-        return (channelTime - reducedChannelTime).coerceAtLeast(1)
     }
 }
