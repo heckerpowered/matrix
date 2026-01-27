970df13762d2c707970c6d276b7638f9087893e5
heckerpowered <72550834+heckerpowered@users.noreply.github.com>
2026-01-27 15:12:49 +0530

    feat(matrix): Add MagicDataSpecification for polymorphic serialization

---
+++ src/main/kotlin/heckerpowered/matrix/common/magic/MagicDataSpecification.kt
@@ -0,0 +1,29 @@
+/*
+ * SPDX-License-Identifier: MIT
+ * Copyright (c) 2026 heckerpowered
+ */
+
+package heckerpowered.matrix.common.magic
+
+import kotlinx.serialization.InternalSerializationApi
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.modules.SerializersModule
+import kotlinx.serialization.modules.polymorphic
+import kotlinx.serialization.serializer
+import kotlin.reflect.KClass
+
+interface MagicDataSpecification {
+    fun serializerModule(): SerializersModule
+}
+
+@OptIn(InternalSerializationApi::class)
+inline fun <reified T : MagicData> MagicDataSpecification(
+    noinline serializer: () -> KSerializer<T> = { T::class.serializer() },
+): MagicDataSpecification = object : MagicDataSpecification {
+    override fun serializerModule(): SerializersModule = SerializersModule {
+        polymorphic(MagicData::class) {
+            @Suppress("UNCHECKED_CAST")
+            subclass(T::class as KClass<MagicData>, serializer() as KSerializer<MagicData>)
+        }
+    }
+}
