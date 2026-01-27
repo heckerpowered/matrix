7e215ca4a7b38b8d469196b6fe3a5a22a1b72e2e
heckerpowered <72550834+heckerpowered@users.noreply.github.com>
2026-01-27 15:05:10 +0530

    refactor(matrix): update SculkCatalystMagic to use new entity search and damage methods

--- src/main/kotlin/heckerpowered/matrix/common/magic/SculkCatalystMagic.kt
+++ src/main/kotlin/heckerpowered/matrix/common/magic/SculkCatalystMagic.kt
@@ -1,37 +1,37 @@
 /*
  * SPDX-License-Identifier: MIT
- * Copyright (c) 2025 heckerpowered
+ * Copyright (c) 2026 heckerpowered
  */
 
 package heckerpowered.matrix.common.magic
 
 import heckerpowered.matrix.Matrix
-import heckerpowered.matrix.common.effect.bloodPactActive
+import heckerpowered.matrix.common.effect.isBloodPactActive
 import heckerpowered.matrix.common.event.ReadDataCallback
 import heckerpowered.matrix.common.event.WriteDataCallback
+import heckerpowered.matrix.common.magic.ChannelQueue.Companion.getChannelQueue
 import heckerpowered.matrix.common.magic.GameTick.Companion.ticks
 import heckerpowered.matrix.common.magic.Mana.Companion.mana
 import heckerpowered.matrix.common.network.ExplosionPayload
-import heckerpowered.matrix.common.persistent.ChannelQueue
-import heckerpowered.matrix.common.persistent.getChannelSequence
-import heckerpowered.matrix.common.tag.MatrixDamageTypes
-import heckerpowered.matrix.core.getNearestEntities
+import heckerpowered.matrix.core.common.balance.Accumulator
+import heckerpowered.matrix.core.extensions.EntityExtensions.damage
 import heckerpowered.matrix.core.killed
+import heckerpowered.matrix.core.utility.EntitySearch.getNearestEntities
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.modules.SerializersModule
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking
 import net.minecraft.entity.LivingEntity
 import net.minecraft.entity.boss.WitherEntity
 import net.minecraft.entity.boss.dragon.EnderDragonEntity
 import net.minecraft.entity.damage.DamageTypes
+import net.minecraft.entity.mob.WardenEntity
 import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.nbt.NbtCompound
 import net.minecraft.nbt.NbtElement
 import net.minecraft.nbt.NbtInt
 import net.minecraft.nbt.NbtList
-import net.minecraft.particle.ParticleTypes
 import net.minecraft.server.network.ServerPlayerEntity
 import net.minecraft.server.world.ServerWorld
-import net.minecraft.sound.SoundCategory
-import net.minecraft.sound.SoundEvents
 
 object SculkCatalystMagic : Magic(
     MagicDefinition(
@@ -39,23 +39,23 @@ object SculkCatalystMagic : Magic(
         12.mana,
         (9 * 20).ticks
     )
-) {
+), MagicDataSpecification {
+
+    @Serializable
     private class SculkCatalystMagicData(
         var bounces: Long = 0,
-        tag: NbtCompound = NbtCompound(),
-    ) : MagicData(tag = tag) {
-        companion object {
-            const val BOUNCES_TAG = "Bounces"
-        }
+    ) : MagicData()
 
-        override fun readFromTag() {
-            super.readFromTag()
-            bounces = tag.getLong(BOUNCES_TAG)
-        }
-
-        override fun writeToTag() {
-            super.writeToTag()
-            tag.putLong(BOUNCES_TAG, bounces)
+    private class SculkCatalystChannelPlan(
+        bypassLock: Boolean = false,
+        costMana: Boolean = true,
+        data: MagicData = MagicData(),
+    ) : ChannelPlan(bypassLock, costMana, data) {
+        override fun isMagicAvailable(availableStatus: MagicAvailableStatus): Boolean {
+            if (availableStatus == MagicAvailableStatus.SCULK_CATALYST_IS_ALREADY_ACTIVE) {
+                return true
+            }
+            return super.isMagicAvailable(availableStatus)
         }
     }
 
@@ -94,8 +94,8 @@ object SculkCatalystMagic : Magic(
         val idList = matrixNbt.getList("TrackedEntityIds", NbtElement.INT_TYPE.toInt())
 
         val resolvedEntities = mutableListOf<LivingEntity>()
-        for (i in 0 until idList.size) {
-            val id = (idList[i] as? NbtInt)?.intValue() ?: continue
+        for (element in idList) {
+            val id = (element as? NbtInt)?.intValue() ?: continue
             val tracked = world.getEntityById(id) as? LivingEntity
             if (tracked != null) {
                 resolvedEntities.add(tracked)
@@ -108,8 +108,8 @@ object SculkCatalystMagic : Magic(
     private val sculkCatalystTracker = mutableMapOf<PlayerEntity, MutableList<LivingEntity>>()
     private val lock = Any()
 
-    override fun channel(player: PlayerEntity, target: LivingEntity, sequence: ChannelQueue, data: MagicData) {
-        super.channel(player, target, sequence, data)
+    override fun channel(player: PlayerEntity, target: LivingEntity, queue: ChannelQueue, data: MagicData) {
+        super.channel(player, target, queue, data)
         synchronized(lock) {
             sculkCatalystTracker.computeIfAbsent(player) {
                 mutableListOf()
@@ -119,29 +119,30 @@ object SculkCatalystMagic : Magic(
 
     override fun cast(player: ServerPlayerEntity?, target: LivingEntity, sequence: ChannelQueue, data: MagicData) {
         super.cast(player, target, sequence, data)
-        val sculkCatalystData = data as? SculkCatalystMagicData ?: SculkCatalystMagicData(tag = data.tag)
+        val sculkCatalystData = data as? SculkCatalystMagicData ?: SculkCatalystMagicData()
+        sculkCatalystData.copyFrom(data)
         val bounces = ++sculkCatalystData.bounces
 
-        val damageSource = MemoryEraseMagic.getDamageSource(player, target, sequence) { player?.damageSources?.create(MatrixDamageTypes.magic, player) }
-        if (target !is WitherEntity && target !is EnderDragonEntity) {
+        val damageSource = MemoryWipeMagic.getDamageSource(player, target, data) { player?.damageSources?.create(DamageTypes.OUT_OF_WORLD, player) }
+        if (target !is WitherEntity && target !is EnderDragonEntity && target !is WardenEntity) {
             target.health = .0F
             target.killed = true
             target.damage(target.damageSources.create(DamageTypes.OUT_OF_WORLD, player), Float.POSITIVE_INFINITY)
             target.onDeath(damageSource)
         } else {
-            target.damage(damageSource, target.maxHealth * 4.0F)
+            target.damage(target.maxHealth * 4.0F, damageSource)
         }
         if (!target.isAlive) {
             (target.world as? ServerWorld)?.apply {
-                repeat(10) {
-                    spawnParticles(ParticleTypes.SCULK_SOUL, target.x, target.y, target.z, 20, 0.1, .1, 0.1, 0.25)
-                }
-                repeat(128) {
-                    spawnParticles(ParticleTypes.SONIC_BOOM, target.x, target.y + it, target.z, 1, 0.0, 0.0, 0.0, 0.0)
-                }
-
-                playSound(null, target.x, target.y, target.z, SoundEvents.BLOCK_SCULK_SENSOR_CLICKING, SoundCategory.PLAYERS, 1.0F, 1.0F, random.nextLong())
-                playSound(null, target.x, target.y, target.z, SoundEvents.BLOCK_SCULK_SHRIEKER_SHRIEK, SoundCategory.PLAYERS, 1.0F, 1.0F, random.nextLong())
+                // repeat(10) {
+                //     spawnParticles(ParticleTypes.SCULK_SOUL, target.x, target.y, target.z, 20, 0.1, .1, 0.1, 0.25)
+                // }
+                // repeat(128) {
+                //     spawnParticles(ParticleTypes.SONIC_BOOM, target.x, target.y + it, target.z, 1, 0.0, 0.0, 0.0, 0.0)
+                // }
+//
+                // playSound(null, target.x, target.y, target.z, SoundEvents.BLOCK_SCULK_SENSOR_CLICKING, SoundCategory.PLAYERS, 1.0F, 1.0F, random.nextLong())
+                // playSound(null, target.x, target.y, target.z, SoundEvents.BLOCK_SCULK_SHRIEKER_SHRIEK, SoundCategory.PLAYERS, 1.0F, 1.0F, random.nextLong())
 
                 target.world.server?.playerManager?.playerList?.forEach {
                     ServerPlayNetworking.send(it, ExplosionPayload(target.id))
@@ -153,29 +154,50 @@ object SculkCatalystMagic : Magic(
             return
         }
 
-        val nearestEntity = target.getNearestEntities(20.0) {
-            it is LivingEntity
-                    && (it.getChannelSequence(player)?.channelingMagicCount() ?: 0) == 0
-                    && it != player
-                    && it.isAlive
-        } as? LivingEntity ?: return
-        ChannelQueue.channelMagic(SculkCatalystMagic, player, nearestEntity, true, data = sculkCatalystData)
+        val nearestEntity = target.getNearestEntities(20.0)
+            .filterIsInstance<LivingEntity>()
+            .filter { it != player && it.isAlive }
+            .firstOrNull { it.getChannelQueue(player)?.isEmpty ?: true }
+        if (nearestEntity != null) {
+            ChannelExecutor.channel(
+                SculkCatalystMagic, player, nearestEntity, SculkCatalystChannelPlan(
+                    data = sculkCatalystData
+                )
+            )
+        }
     }
 
     override fun getBaseCost(player: PlayerEntity, target: LivingEntity?, sequence: ChannelQueue?, data: MagicData): Long {
+        val normalCost = when (target) {
+            is EnderDragonEntity, is WitherEntity -> 110
+            is PlayerEntity, is WardenEntity -> 180
+            else -> getNormalCost()
+        }
         val bounces = (data as? SculkCatalystMagicData)?.bounces ?: 0
-        return getNormalCost() + bounces.coerceAtMost(5) * 6
+        return normalCost + bounces.coerceAtMost(5) * 6
     }
 
-    override fun getCost(player: PlayerEntity, target: LivingEntity?, sequence: ChannelQueue?, data: MagicData): Long {
-        if (player.bloodPactActive) {
-            return (super.getCost(player, target, sequence, data) * 0.5).toLong()
+    override fun getCost(player: PlayerEntity, target: LivingEntity?, sequence: ChannelQueue?, data: MagicData, accumulator: Accumulator): Long {
+        if (player.isBloodPactActive) {
+            accumulator.pushCostReduction(0.5)
         }
-        return super.getCost(player, target, sequence, data)
+        return super.getCost(player, target, sequence, data, accumulator)
+    }
+
+    override fun getMagicResistance(player: PlayerEntity, target: LivingEntity?, sequence: ChannelQueue?, data: MagicData): Double {
+        if (target is EnderDragonEntity || target is WitherEntity || target is WardenEntity) {
+            return .0
+        }
+        return super.getMagicResistance(player, target, sequence, data)
     }
 
     override fun getBaseChannelTime(player: PlayerEntity, target: LivingEntity, sequence: ChannelQueue?, data: MagicData): Long {
         val bounces = (data as? SculkCatalystMagicData)?.bounces ?: 0
+        val additionChannelTime = when (target) {
+            is EnderDragonEntity, is WitherEntity -> 8 * 20L
+            is WardenEntity -> 8 * 20L
+            else -> 0L
+        }
         return when (bounces) {
             0L -> 9 * 20
             1L -> 110 // 5.5 * 20 = 5.5s
@@ -184,13 +206,13 @@ object SculkCatalystMagic : Magic(
             4L -> 20
             5L -> 10 // 0.5s
             else -> 10 // 0.5s
-        }
+        } + additionChannelTime
     }
 
     override fun availableStatus(player: PlayerEntity, target: LivingEntity?, sequence: ChannelQueue?): MagicAvailableStatus {
         sculkCatalystTracker[player]?.removeIf {
-            val sequence = player.getChannelSequence(it) ?: return@removeIf true
-            return@removeIf !it.isAlive || sequence.channelingMagicCount() == 0
+            val sequence = player.getChannelQueue(it) ?: return@removeIf true
+            return@removeIf !it.isAlive || sequence.isEmpty
         }
 
         val sculkCatalystIsAlreadyActive = isSculkCatalystActive(player)
@@ -203,8 +225,11 @@ object SculkCatalystMagic : Magic(
     fun isSculkCatalystActive(player: PlayerEntity): Boolean {
         synchronized(lock) {
             return sculkCatalystTracker[player]?.any {
-                it.isAlive && (it.getChannelSequence(player)?.channelingMagics()?.firstOrNull()?.magic == SculkCatalystMagic)
+                it.isAlive && (it.getChannelQueue(player)?.channelingMagics()?.firstOrNull()?.magic == SculkCatalystMagic)
             } == true
         }
     }
+
+    override fun serializerModule(): SerializersModule =
+        MagicDataSpecification<SculkCatalystMagicData>().serializerModule()
 }
