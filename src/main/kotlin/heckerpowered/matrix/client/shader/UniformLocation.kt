80c0e4641ad35ee71bb7a45bdbe21690bcb74f28
heckerpowered <72550834+heckerpowered@users.noreply.github.com>
2026-01-27 15:03:40 +0530

    feat(matrix): Add UniformLocation data class for shader uniform management

---
+++ src/main/kotlin/heckerpowered/matrix/client/shader/UniformLocation.kt
@@ -0,0 +1,11 @@
+/*
+ * SPDX-License-Identifier: MIT
+ * Copyright (c) 2026 heckerpowered
+ */
+
+package heckerpowered.matrix.client.shader
+
+data class UniformLocation(
+    val programObjectId: Int,
+    val locationIndex: Int,
+)
