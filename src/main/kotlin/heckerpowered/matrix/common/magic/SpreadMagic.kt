b5441cda5d6fe7250406ba7b2effcc1a82f9fec6
heckerpowered <72550834+heckerpowered@users.noreply.github.com>
2026-01-27 15:04:21 +0530

    refactor(matrix): remove unused import and simplify SpreadMagic logic

--- src/main/kotlin/heckerpowered/matrix/common/magic/SpreadMagic.kt
+++ src/main/kotlin/heckerpowered/matrix/common/magic/SpreadMagic.kt
@@ -1,6 +1,6 @@
 /*
  * SPDX-License-Identifier: MIT
- * Copyright (c) 2025 heckerpowered
+ * Copyright (c) 2026 heckerpowered
  */
 
 package heckerpowered.matrix.common.magic
@@ -8,7 +8,6 @@ package heckerpowered.matrix.common.magic
 import heckerpowered.matrix.Matrix
 import heckerpowered.matrix.common.magic.GameTick.Companion.ticks
 import heckerpowered.matrix.common.magic.Mana.Companion.mana
-import heckerpowered.matrix.common.persistent.ChannelQueue
 import net.minecraft.entity.LivingEntity
 import net.minecraft.server.network.ServerPlayerEntity
 
@@ -23,19 +22,6 @@ object SpreadMagic : Magic(
         super.cast(player, target, sequence, data)
         if (player == null) {
             return
-        }
-        val magics = sequence.magics.filterIndexed { index, channelingMagic ->
-            index > sequence.index && channelingMagic.magic != this
-        }.map {
-            it.magic
-        }
-        target.world.getOtherEntities(player, target.boundingBox.expand(24.0)).forEach {
-            if (it !is LivingEntity || it == target) {
-                return@forEach
-            }
-            for (magic in magics) {
-                ChannelQueue.channelMagic(magic, player, it)
-            }
         }
     }
 }
