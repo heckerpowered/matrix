ce8884e9b6e7778d9e863acdbd71929a543a05d5
heckerpowered <72550834+heckerpowered@users.noreply.github.com>
2026-01-27 15:20:12 +0530

    add(matrix): Add method to damage entity with custom timeUntilRegen handling

--- src/main/kotlin/heckerpowered/matrix/core/extensions/EntityExtensions.kt
+++ src/main/kotlin/heckerpowered/matrix/core/extensions/EntityExtensions.kt
@@ -1,15 +1,24 @@
 /*
  * SPDX-License-Identifier: MIT
- * Copyright (c) 2025 heckerpowered
+ * Copyright (c) 2026 heckerpowered
  */
 
 package heckerpowered.matrix.core.extensions
 
 import heckerpowered.matrix.core.MatrixMath.eulerToQuaternion
 import net.minecraft.entity.Entity
+import net.minecraft.entity.damage.DamageSource
 import org.joml.Quaternionf
 
 object EntityExtensions {
     val Entity.rotation: Quaternionf
         get() = eulerToQuaternion(yaw, pitch)
+
+    fun Entity.damage(amount: Float, damageSource: DamageSource): Boolean {
+        val previous = timeUntilRegen
+        timeUntilRegen = 0
+        val result = damage(damageSource, amount)
+        timeUntilRegen = previous
+        return result
+    }
 }
