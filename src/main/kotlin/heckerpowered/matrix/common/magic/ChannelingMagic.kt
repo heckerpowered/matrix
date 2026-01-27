0d4f8faa9f2719f0af1615a3487c4028058c0b73
heckerpowered <72550834+heckerpowered@users.noreply.github.com>
2026-01-27 13:44:32 +0530

    feat(matrix): Add ChannelingMagic data class for channeling magic properties

---
+++ src/main/kotlin/heckerpowered/matrix/common/magic/ChannelingMagic.kt
@@ -0,0 +1,19 @@
+/*
+ * SPDX-License-Identifier: MIT
+ * Copyright (c) 2026 heckerpowered
+ */
+
+package heckerpowered.matrix.common.magic
+
+import kotlinx.serialization.Contextual
+import kotlinx.serialization.Polymorphic
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class ChannelingMagic(
+    @Contextual val magic: Magic,
+    val cost: Long,
+    val channelTime: Long,
+    var currentChannelTime: Long = 0,
+    @Polymorphic val data: MagicData = MagicData(),
+)
