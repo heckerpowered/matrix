b9fe4903919e048843b7540d88d288397b76433e
heckerpowered <72550834+heckerpowered@users.noreply.github.com>
2026-01-27 15:11:03 +0530

    feat(matrix): Update mana methods to return Mana type and add isInfiniteMana method

--- src/main/kotlin/heckerpowered/matrix/client/MatrixClientProxy.kt
+++ src/main/kotlin/heckerpowered/matrix/client/MatrixClientProxy.kt
@@ -1,27 +1,33 @@
 /*
  * SPDX-License-Identifier: MIT
- * Copyright (c) 2025 heckerpowered
+ * Copyright (c) 2026 heckerpowered
  */
 
 package heckerpowered.matrix.client
 
 import heckerpowered.matrix.common.MatrixCommonProxy
+import heckerpowered.matrix.common.magic.Mana
+import heckerpowered.matrix.common.magic.Mana.Companion.mana
 import net.minecraft.entity.player.PlayerEntity
 
 class MatrixClientProxy : MatrixCommonProxy() {
-    override fun getPlayerMana(player: PlayerEntity): Double {
+    override fun getPlayerMana(player: PlayerEntity): Mana {
         if (minecraft.player == null || player != ::player.get()) {
-            return .0
+            return .0.mana
         }
 
-        return MatrixHud.mana - MatrixHud.manaUsage
+        return (MatrixHud.mana - MatrixHud.manaUsage).mana
     }
 
-    override fun getPlayerMaxMana(player: PlayerEntity): Double {
+    override fun getPlayerMaxMana(player: PlayerEntity): Mana {
         if (minecraft.player == null || player != ::player.get()) {
-            return .0
+            return .0.mana
         }
 
-        return MatrixHud.maxMana
+        return MatrixHud.maxMana.mana
+    }
+
+    override fun isInfiniteMana(player: PlayerEntity): Boolean {
+        return false
     }
 }
