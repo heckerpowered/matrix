ba7fa99c40143ea9aa14334fa707aeeff5474b3f
heckerpowered <72550834+heckerpowered@users.noreply.github.com>
2026-01-27 15:04:02 +0530

    refactor(matrix): remove unused import in TargetPositioningMagic.kt

--- src/main/kotlin/heckerpowered/matrix/common/magic/TargetPositioningMagic.kt
+++ src/main/kotlin/heckerpowered/matrix/common/magic/TargetPositioningMagic.kt
@@ -1,6 +1,6 @@
 /*
  * SPDX-License-Identifier: MIT
- * Copyright (c) 2025 heckerpowered
+ * Copyright (c) 2026 heckerpowered
  */
 
 package heckerpowered.matrix.common.magic
@@ -8,7 +8,6 @@ package heckerpowered.matrix.common.magic
 import heckerpowered.matrix.Matrix
 import heckerpowered.matrix.common.magic.GameTick.Companion.ticks
 import heckerpowered.matrix.common.magic.Mana.Companion.mana
-import heckerpowered.matrix.common.persistent.ChannelQueue
 import net.minecraft.entity.LivingEntity
 import net.minecraft.entity.effect.StatusEffectInstance
 import net.minecraft.entity.effect.StatusEffects
