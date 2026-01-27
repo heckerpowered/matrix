74c2bf73ba70b1e05616023fb03d07f850f3becb
heckerpowered <72550834+heckerpowered@users.noreply.github.com>
2026-01-27 15:13:23 +0530

    feat(matrix): Add MagicContext for managing magic operations with player, target, and channel queue

---
+++ src/main/kotlin/heckerpowered/matrix/common/magic/MagicContext.kt
@@ -0,0 +1,15 @@
+/*
+ * SPDX-License-Identifier: MIT
+ * Copyright (c) 2026 heckerpowered
+ */
+
+package heckerpowered.matrix.common.magic
+
+import net.minecraft.entity.LivingEntity
+import net.minecraft.entity.player.PlayerEntity
+
+data class MagicContext(
+    val player: PlayerEntity,
+    val target: LivingEntity,
+    val channelQueue: ChannelQueue?,
+)
