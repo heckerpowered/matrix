8e55f7b5cad74e810e27fcfcdad04601109f4ff2
heckerpowered <72550834+heckerpowered@users.noreply.github.com>
2026-01-27 15:11:49 +0530

    feat(matrix): Add serialization to ManaData class

--- src/main/kotlin/heckerpowered/matrix/common/persistent/ManaData.kt
+++ src/main/kotlin/heckerpowered/matrix/common/persistent/ManaData.kt
@@ -1,8 +1,11 @@
 /*
  * SPDX-License-Identifier: MIT
- * Copyright (c) 2025 heckerpowered
+ * Copyright (c) 2026 heckerpowered
  */
 
 package heckerpowered.matrix.common.persistent
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class ManaData(var mana: Double, var maxMana: Double, var isInfinite: Boolean = false)
