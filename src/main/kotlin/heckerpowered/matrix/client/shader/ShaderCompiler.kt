52ef5dcedea4248a11eaf20e2d8494123e650a54
heckerpowered <72550834+heckerpowered@users.noreply.github.com>
2026-01-27 15:04:58 +0530

    refactor(matrix): simplify ShaderCompiler and move old implementation to ShaderCompilerV1

--- src/main/kotlin/heckerpowered/matrix/client/shader/ShaderCompiler.kt
+++ src/main/kotlin/heckerpowered/matrix/client/shader/ShaderCompiler.kt
@@ -1,78 +1,34 @@
 /*
  * SPDX-License-Identifier: MIT
- * Copyright (c) 2025 heckerpowered
+ * Copyright (c) 2026 heckerpowered
  */
 
 package heckerpowered.matrix.client.shader
 
 import com.mojang.blaze3d.platform.GlConst
-import heckerpowered.matrix.client.render.OpenGLExtensions
+import heckerpowered.matrix.client.render.MatrixRenderSystem
 import heckerpowered.matrix.client.shader.cache.ShaderCompilationException
-import kotlinx.coroutines.*
-import org.lwjgl.glfw.GLFW
-import org.lwjgl.opengl.GL46.*
-import org.lwjgl.system.MemoryStack
-import org.lwjgl.system.MemoryUtil
+import org.lwjgl.opengl.ARBShadingLanguageInclude
+import org.lwjgl.opengl.GL20.*
 
 object ShaderCompiler {
-    private fun shaderSource(shader: Int, source: String) {
-        try {
-            MemoryStack.stackPush().use { memoryStack ->
-                val sourceBuffer = memoryStack.UTF8(source, false)
-                val pointerBuffer = memoryStack.mallocPointer(1)
-                    .put(sourceBuffer)
-                    .flip()
-                val lengthBuffer = memoryStack.mallocInt(1)
-                    .put(sourceBuffer.capacity())
-                    .flip()
-                glShaderSource(shader, pointerBuffer, lengthBuffer)
-                return
-            }
-        } catch (exception: OutOfMemoryError) {
-            exception.printStackTrace()
-        }
+    fun compileShader(descriptor: ShaderSourceDescriptor): Int {
+        val shader = MatrixRenderSystem.createShader(descriptor.stage.shaderType)
 
-        val sourceBuffer = MemoryUtil.memUTF8(source, false)
-        try {
-            MemoryStack.stackPush().use { memoryStack ->
-                val pointerBuffer = memoryStack.mallocPointer(1)
-                    .put(sourceBuffer)
-                    .flip()
-                val lengthBuffer = memoryStack.mallocInt(1)
-                    .put(sourceBuffer.capacity())
-                    .flip()
-                glShaderSource(shader, pointerBuffer, lengthBuffer)
-            }
-        } finally {
-            MemoryUtil.memFree(sourceBuffer)
-        }
-    }
-
-    fun compileShader(source: String, type: Int): Int {
-        val shader = glCreateShader(type)
-        glShaderSource(shader, source)
+        glShaderSource(shader, descriptor.source)
         glCompileShader(shader)
 
         if (glGetShaderi(shader, GlConst.GL_COMPILE_STATUS) == GlConst.GL_FALSE) {
             val logLength = glGetShaderi(shader, GL_INFO_LOG_LENGTH)
             val log = glGetShaderInfoLog(shader, logLength)
-            throw ShaderCompilationException("$log\n$source")
+            throw ShaderCompilationException("$log\n${descriptor.source}")
         }
 
         return shader
     }
 
-    @OptIn(DelicateCoroutinesApi::class)
-    val Dispatcher = newFixedThreadPoolContext(1, "Shader-Compile")
-
-    private fun ensureGLContext() {
-        if (GLFW.glfwGetCurrentContext() == 0L) {
-            OpenGLExtensions.initGLContext("Shader-Compile")
-        }
-    }
-
-    fun CoroutineScope.compileShaderAsync(source: String, type: Int): Deferred<Int> = async(Dispatcher) {
-        ensureGLContext()
-        compileShader(source, type)
+    fun addInclude(virtualPath: String, shaderSource: String) {
+        // println("Add include: $virtualPath")
+        ARBShadingLanguageInclude.glNamedStringARB(ARBShadingLanguageInclude.GL_SHADER_INCLUDE_ARB, virtualPath, shaderSource)
     }
 }
