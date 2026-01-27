7765f21502a7322aa5c2dc335b298a64d7eea094
heckerpowered <72550834+heckerpowered@users.noreply.github.com>
2026-01-27 15:02:59 +0530

    refactor(matrix): update CrippleMovementMagic to use Accumulator for cost calculation

--- src/main/kotlin/heckerpowered/matrix/common/magic/CrippleMovementMagic.kt
+++ src/main/kotlin/heckerpowered/matrix/common/magic/CrippleMovementMagic.kt
@@ -1,6 +1,6 @@
 /*
  * SPDX-License-Identifier: MIT
- * Copyright (c) 2025 heckerpowered
+ * Copyright (c) 2026 heckerpowered
  */
 
 package heckerpowered.matrix.common.magic
@@ -9,7 +9,7 @@ import heckerpowered.matrix.Matrix
 import heckerpowered.matrix.common.effect.MatrixStatusEffects.CRIPPLE_MOVEMENT_EFFECT
 import heckerpowered.matrix.common.magic.GameTick.Companion.ticks
 import heckerpowered.matrix.common.magic.Mana.Companion.mana
-import heckerpowered.matrix.common.persistent.ChannelQueue
+import heckerpowered.matrix.core.common.balance.Accumulator
 import net.minecraft.entity.LivingEntity
 import net.minecraft.entity.effect.StatusEffectInstance
 import net.minecraft.entity.player.PlayerEntity
@@ -54,11 +54,11 @@ object CrippleMovementMagic : Magic(
         return super.availableStatus(player, target, sequence)
     }
 
-    override fun getCost(player: PlayerEntity, target: LivingEntity?, sequence: ChannelQueue?, data: MagicData): Long {
+    override fun getCost(player: PlayerEntity, target: LivingEntity?, sequence: ChannelQueue?, data: MagicData, accumulator: Accumulator): Long {
         if (target is PlayerEntity) {
-            return super.getCost(player, target, sequence, data) * 3
+            return super.getCost(player, target, sequence, data, accumulator) * 3
         }
 
-        return super.getCost(player, target, sequence, data)
+        return super.getCost(player, target, sequence, data, accumulator)
     }
 }
